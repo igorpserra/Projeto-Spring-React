af8268b1c00726b72411c7a41ea0f657672c08a8
Igor P <igor.serra@ssp.ma.gov.br>
2021-11-03 11:44:17 -0300

    Criação do Projeto

---
+++ backend/src/test/java/com/igorp/appvendas/AppvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorp.appvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
