ab7c75a906b872928eb000d23c0eb831fdbce24e
Igor P <igorpserra@gmail.com>
2021-11-07 23:02:41 -0300

    Domain model, database seed

--- backend/src/main/java/com/igorp/appvendas/entities/Sale.java
+++ backend/src/main/java/com/igorp/appvendas/entities/Sale.java
@@ -1,21 +1,22 @@
 package com.igorp.appvendas.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "")
+@Table(name = "tb_sales")
 public class Sale {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer visited;
     private Integer deals;
     private Double amount;
     private LocalDate date;
 
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
     private Seller seller;
 
     public Sale(){}
--- backend/src/main/java/com/igorp/appvendas/entities/Seller.java
+++ backend/src/main/java/com/igorp/appvendas/entities/Seller.java
@@ -1,14 +1,21 @@
 package com.igorp.appvendas.entities;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "tb_sellers")
 public class Seller {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy = "seller")
     private List<Sale> sales = new ArrayList<>();
+
 
     public Seller(){}
 
