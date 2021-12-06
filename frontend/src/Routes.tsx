20c5bc779d908af7dd90f413aad67666cd47c8b5
igorpserra <igorpserra@gmail.com>
2021-12-06 16:57:29 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,20 @@
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function MyRoutes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route path="/">
+                    <Dashboard/>
+                </Route>
+            </Switch> 
+        </BrowserRouter>
+    );
+}
+
+export default MyRoutes;
