33af3bebf1263f94204ed1d5e1cd18a1e3a967e3
igorpserra <igorpserra@gmail.com>
2021-12-07 15:30:13 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,7 +2,7 @@ import Dashboard from "pages/Dashboard";
 import Home from "pages/Home";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function MyRoutes() {
+function Routes() {
     return (
         <BrowserRouter>
             <Switch>
@@ -17,4 +17,4 @@ function MyRoutes() {
     );
 }
 
-export default MyRoutes;
+export default Routes;
