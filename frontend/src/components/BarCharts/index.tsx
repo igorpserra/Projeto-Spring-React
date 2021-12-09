6f34d935d221f2c077cf70d5d0271f64673549b7
igorpserra <igorpserra@gmail.com>
2021-12-09 17:18:11 -0300

    Try BarChart

--- frontend/src/components/BarCharts/index.tsx
+++ frontend/src/components/BarCharts/index.tsx
@@ -1,6 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = 
+    {
+        labels: {
+            categories: string[]
+        },
+        series: [
+            {
+            name: string,    
+            data: number[]
+            }
+        ]
+    };
 
 const BarChart = () => {
+
+    const[chartData, setChartData] = useState<ChartData>({labels: {categories: []},series:[{name: '', data : []}]});
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then((response) => { 
+                const data = response.data as SaleSuccess[];
+                const MyLabels = data.map(x => x.sellerName);
+                const Visited = data.map(x => x.visited);
+                const Deals = data.map(x => x.visited);
+                
+                let SuccessRate = [];
+
+                for(let i = 0; i < Visited.length; i++){
+                    let rate = Visited[i]/Deals[i];
+                    SuccessRate.push(rate); 
+                }
+
+                setChartData({labels: {categories: MyLabels},series:[{name: '% Sucesso', data : SuccessRate}]})
+            })
+    })
+
 
     const options = {
         plotOptions: {
@@ -10,22 +49,22 @@ const BarChart = () => {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
+    // const mockData = {
+    //     labels: {
+    //         categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //     },
+    //     series: [
+    //         {
+    //             name: "% Sucesso",
+    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]
+    //         }
+    //     ]
+    // };
 
     return (
         <Chart   
-            options = {{...options, xaxis : mockData.labels}}
-            series = {mockData.series}
+            options = {{...options, xaxis : chartData.labels}}
+            series = {chartData.series}
             type = "bar"
             height = "240"
         />
